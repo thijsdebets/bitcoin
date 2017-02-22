35ffdbf018b213f914342e294b282950bc9da27e
Thijs Debets <thijs@tdebets.com>
2017-02-22 01:04:11 +0100

    remove , to use numbers above 1000 in domoticz

--- bitcoin.sh
+++ bitcoin.sh
@@ -3,7 +3,7 @@
 # Create two Virtual Sensors of type Custom Sensor
 # 1 sensor with name BitcoinValue and 1 sensor with name BitcoinWallet
 # Enter amount of bitcoins in your wallet here:
-number='0.09372178'
+number='0.00125342'
 # SET the required currency: USD, GBP, EUR
 currency="EUR"
 
@@ -18,11 +18,14 @@ bitcoinwalletidx=317
 # */15 * * * * sleep $(expr $RANDOM \% 600); /home/pi/domoticz/scripts/bitcoin/bitcoin.sh 2>&1
 
 
-bitcoin=$(curl -s http://api.coindesk.com/v1/bpi/currentprice.json | sed 's/","/\n/g' | grep -A2 $currency | grep 'rate"' | cut -d'"' -f3)
+bitcoin=$(curl -s http://api.coindesk.com/v1/bpi/currentprice.json | sed 's/","/\n/g' | grep -A2 $currency | grep 'rate"' | cut -d'"' -f3 | tr -d ",")
 echo $bitcoin
 wallet=$(awk "BEGIN {print $number*$bitcoin}")
 echo $wallet
 if [ "$bitcoin" != "" ] && [ "${bitcoin%.*}" -gt "100" ] ; then
 	curl "http://127.0.0.1:8080/json.htm?type=command&param=udevice&idx=$bitcoinvalueidx&nvalue=0&svalue=$bitcoin;0"
 	curl "http://127.0.0.1:8080/json.htm?type=command&param=udevice&idx=$bitcoinwalletidx&nvalue=0&svalue=$wallet;0"
+
+	curl "http://127.0.0.1:8080/json.htm?type=command&param=udevice&idx=300&nvalue=0&svalue=$bitcoin;0"
+	curl "http://127.0.0.1:8080/json.htm?type=command&param=udevice&idx=309&nvalue=0&svalue=$wallet;0"
 fi
